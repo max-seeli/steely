2bd97b49bfaf3d8f797eec363e1c1b32d66ed2f9
Max Seeliger <maximilian.seeliger@gmail.com>
2025-03-27 23:14:20 +0100

    feat: EDA of subtask 1

--- src/steely/__init__.py
+++ src/steely/__init__.py
@@ -1,1 +1,7 @@
+from pathlib import Path
 
+MODULE_DIR = Path(__file__).resolve().parent
+ROOT_DIR = MODULE_DIR.parent.parent
+DATA_DIR = ROOT_DIR / "data"
+DATA_TASK_1_DIR = DATA_DIR / "pan25-generative-ai-detection-task1-train"
+DATA_TASK_2_DIR = DATA_DIR / "pan25-generative-ai-detection-task2-train"
